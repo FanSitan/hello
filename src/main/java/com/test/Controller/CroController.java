84d1dbac606927380e35857da93cb925891cc01a
Administrator <444957897@qq.com>
2017-11-08 11:16:29 +0800

    print()

--- src/main/java/com/test/Controller/CroController.java
+++ src/main/java/com/test/Controller/CroController.java
@@ -4,6 +4,9 @@ package com.test.Controller;
  * Created by Administrator on 2017/11/8 0008.
  */
 public class CroController {
+    public String print(){
+        return "ab";
+    }
     public int sum(){
         return 3;
     }
